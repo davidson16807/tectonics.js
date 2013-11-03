7e14c31a2c57062a809ef6776a74c4e10fd7d0d2
Carl Davidson <diadophus@gmail.com>
2013-11-03 08:55:39 -0600

    Omitted conflict correction causing javascript exception.

--- Plate.js
+++ Plate.js
@@ -18,12 +18,7 @@ function Plate(world, center, eulerPole, angularSpeed)
 	this._crust = world.crust;
 	this._geometry = world.grid.initializer(world.NA);
 	this._vertices = this._geometry.vertices;
-<<<<<<< HEAD
 	this._material	= new THREE.MeshBasicMaterial({color: 0x506e1e, transparent:false, opacity:1});
-=======
-	this._material	= new THREE.MeshBasicMaterial({color: Math.random() * 0xffffff, transparent:true, opacity:1});
-	this._neighbors = [];
->>>>>>> master
 	this.mesh	= new THREE.Mesh( this._geometry, this._material ); 
 	
 	for(var i = 0, length = this._vertices.length, vertices = this._vertices; i<length; i++){
