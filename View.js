b4775e95da1e9697da35c3f3290d6de15fd4484d
Carl Davidson <diadophus@gmail.com>
2013-11-27 15:09:25 -0600

    Created Style class to encapsulate differences in presentation between master and gh-pages branch

--- View.js
+++ View.js
@@ -2,7 +2,58 @@ var _hashPlate = function(plate){
 	return plate.mesh.uuid
 }
 
-function View(world){
+function Style(getWorldLayers, getPlateLayers){
+	this.getWorldLayers = getWorldLayers
+	this.getPlateLayers = getPlateLayers
+}
+satelliteStyle = new Style(
+function(view){
+	var geometry	= world.grid.initializer(view.THRESHOLD);
+	asthenosphere	= new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({color:0x000000}) );
+	asthenosphere.renderDepth = -1;
+	view.scene.add(asthenosphere);
+	
+	var geometry	= world.grid.initializer(view.SEALEVEL);
+	ocean	= new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({color:0x0a0a32}) ); 
+	ocean.renderDepth = -2;
+	view.scene.add(ocean);
+},
+function(view, plate){
+	var sealevel = view.world.SEALEVEL
+	var epipelagic = sealevel - 200;
+	var mountain = sealevel + 5000;
+	var land = new Layer(view, 1.05, 
+		new THREE.MeshBasicMaterial({color: 0x506e1e}), 
+		function(cell){return cell.content.displacement > sealevel});
+	var ice = new Layer(view, 1.06, 
+		new THREE.MeshBasicMaterial({color: 0xffffff}), 
+		function(cell){return cell.content.displacement > epipelagic && Math.abs(plate.mesh.localToWorld(cell.clone()).y) > 0.8});
+	var shallow = new Layer(view, 1.04, 
+		new THREE.MeshBasicMaterial({color: 0x0a968c}), 
+		function(cell){return cell.content.displacement > epipelagic});
+	return [land, ice, shallow];
+});
+debugStyle = new Style(
+function(view){
+	var geometry	= world.grid.initializer(view.THRESHOLD);
+	asthenosphere	= new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({color:0x000000}) );
+	asthenosphere.renderDepth = -1;
+	view.scene.add(asthenosphere);
+	
+	var geometry	= world.grid.initializer(view.SEALEVEL);
+	ocean	= new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({color:0x0a0a32, transparent:true, opacity:0.5}) ); 
+	ocean.renderDepth = -2;
+	view.scene.add(ocean);
+},
+function(view, plate){
+	var sealevel = view.world.SEALEVEL
+	var land = new Layer(view, 1.05, 
+		new THREE.MeshBasicMaterial({color: random.random()*0xffffff}), 
+		function(cell){return cell.content.displacement > sealevel});
+	return [land];
+});
+
+function View(world, style){
 	this.NA = 0.1;
 	this.THRESHOLD = 1.0;
 	this.SUBDUCTED = 1.01;
@@ -10,6 +61,7 @@ function View(world){
 	this.SEALEVEL = 1.03;
 	this.LAND = 1.04;
 	this.world = world;
+	this.style = style
 	this.meshes = new buckets.Dictionary(_hashPlate);
 
 	// create a scene
@@ -20,15 +72,7 @@ function View(world){
 	this.camera.position.set(0, 0, 5);
 	this.scene.add(this.camera);
 	
-	var geometry	= world.grid.initializer(this.THRESHOLD);
-	this.asthenosphere	= new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({color:0x000000}) );
-	this.asthenosphere.renderDepth = -1;
-	this.scene.add(this.asthenosphere);
-	
-	var geometry	= world.grid.initializer(this.SEALEVEL);
-	this.ocean	= new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({color:0x0a0a32}) ); 
-	this.ocean.renderDepth = -2;
-	this.scene.add(this.ocean);
+	style.getWorldLayers(this);
 }
 
 View.prototype.update = function(){
@@ -41,20 +85,7 @@ View.prototype.update = function(){
 }
 
 View.prototype.add = function(plate){
-	var sealevel = this.world.SEALEVEL
-	var epipelagic = sealevel - 200;
-	var mountain = sealevel + 5000;
-	var land = new Layer(this, 1.05, 
-		new THREE.MeshBasicMaterial({color: 0x506e1e}), 
-		function(cell){return cell.content.displacement > sealevel});
-	var ice = new Layer(this, 1.06, 
-		new THREE.MeshBasicMaterial({color: 0xffffff}), 
-		function(cell){return cell.content.displacement > epipelagic && Math.abs(plate.mesh.localToWorld(cell.clone()).y) > 0.8});
-	var shallow = new Layer(this, 1.04, 
-		new THREE.MeshBasicMaterial({color: 0x0a968c}), 
-		function(cell){return cell.content.displacement > epipelagic});
-	
-	this.meshes.set(plate, [land, ice, shallow]);
+	this.meshes.set(plate, this.style.getPlateLayers(this, plate));
 }
 
 View.prototype.remove = function(plate){
