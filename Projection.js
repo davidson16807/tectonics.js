aead119fd1b52f446a1d71071f2d578f24f33740
Carl Davidson <diadophus@gmail.com>
2014-01-24 23:00:55 -0600

    Prime meridian can now be readjusted on equirectangular projections.

--- Projection.js
+++ Projection.js
@@ -4,6 +4,7 @@ var _multiline = function(f) {
 
 vertexShaders = {};
 vertexShaders.equirectangular = _multiline(function() {/**   
+	const float PI = 3.14;
 	const float OCEAN = 0.0;
 	const float LAND = 0.01;
 	const float NONE = -0.01;
@@ -13,20 +14,28 @@ vertexShaders.equirectangular = _multiline(function() {/**
 	varying vec4 vPosition;
 	uniform float sealevel;
 
+	float lat(vec3 pos) {
+		return atan(-pos.z, pos.x) + PI;
+	}
+	float lon(vec3 pos) {
+		return asin(pos.y / length(pos));
+	}
+
 	void main() {
 		vDisplacement = displacement;
 		vPosition = modelMatrix * vec4( position, 1.0 );
+
 		
 		vec4 modelPos = modelMatrix * vec4( position, 1.0 );
 		float height = displacement > sealevel? LAND : displacement > 1.0? OCEAN : NONE;
 		vec4 displaced = vec4(
-			atan(-modelPos.z, modelPos.x), 
-			asin(modelPos.y / length(modelPos))*2., 
+			mod(lat(modelPos.xyz) + lat(cameraPosition), 2.*PI) - PI,
+			lon(modelPos.xyz), 
 			height, 
 			1);
-		mat4 viewMatrixMod = mat4(1.);
-		viewMatrixMod[3] = viewMatrix[3];
-		gl_Position = projectionMatrix * viewMatrixMod * displaced;
+		mat4 scaleMatrix = mat4(1);
+		scaleMatrix[3] = viewMatrix[3];
+		gl_Position = projectionMatrix * scaleMatrix * displaced;
 	}
 
 **/});
