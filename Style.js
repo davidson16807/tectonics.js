bcb22b54686da11508b8863764bdf6900dcb90a7
Carl Davidson <diadophus@gmail.com>
2014-01-26 19:58:28 -0600

    Tweaked debug shader to display ocean depth since now it matters.

--- Style.js
+++ Style.js
@@ -85,16 +85,14 @@ fragmentShaders.debug = _multiline(function() {/**
 	const vec4 TOP = vec4(1.0,1.0,1.0,1.0);
 
 	void main() {
-		float mountainMinHeight = sealevel ;
+		float mountainMinHeight = sealevel + 5000.;
 		float mountainMaxHeight = sealevel + 15000.0;
-		float nonexistantHeight = -999999.;
-		if(vDisplacement > mountainMinHeight){
+		if(vDisplacement > sealevel){
 			float x = smoothstep(mountainMinHeight, mountainMaxHeight, vDisplacement);
 			gl_FragColor =  mix(vec4(color, 1.0), TOP, x);
-		} else if (vDisplacement > sealevel){
-			gl_FragColor =  vec4(color, 1.0);
 		} else if (vDisplacement > 1.){
-			gl_FragColor =  mix(BOTTOM, vec4(color, 1.0), 0.5);
+			float x = smoothstep(-sealevel, sealevel, vDisplacement);
+			gl_FragColor =  mix(BOTTOM, vec4(color*.75, 1.0), x);
 		} else {
 			gl_FragColor =  vec4(0,0,0,1);
 		}
