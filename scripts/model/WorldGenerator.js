b373c5ef9bd5e9a34120c61dc27f673e446f9f75
Carl Davidson <davids14@illinoisalumni.org>
2015-06-10 22:25:20 -0500

    Added code comments and allocated land based on land:ocean ratio found on earth.

--- scripts/model/WorldGenerator.js
+++ scripts/model/WorldGenerator.js
@@ -38,32 +38,65 @@ EliasWorldGenerator.generate = function (world, optional) {
 		return 2 / (1 + exp(-k*x)) - 1;
 	}
 
-
+	// first, we generate matrices expressing direction of continent centers
+	// Only the z axis is used to determine distance to a continent's center,
+	// so we discard all but the row representing the z axis
+	// this row is stored as a vector, and we take the dot product with the cell pos 
+	// to find the z axis relative to the continent center 
 	var zDotMultipliers = [];
-	for (var i = 0; i < 50; i++) {
+	for (var i = 0; i < 1000; i++) {
 		var basis = Sphere.getRandomBasis();
-		var zDotMultiplier = new THREE.Vector3().fromArray(basis.toArray().slice(8,11));
+		var zDotMultiplier = new THREE.Vector3()
+			.fromArray(basis.toArray().slice(8,11))
+			.multiplyScalar(random.random());
 		zDotMultipliers.push(zDotMultiplier);
 	};
-	console.log(zDotMultipliers)
 
+	// Now, we iterate through the cells and find their "height rank".
+	// This value doesn't translate directly elevation. 
+	// It only represents how cells would rank if sorted by elevation.
+	// This is done so we can later derive elevations that are consistent with earth's.
 	var plate = new Plate(world);
 	for(var i=0, length = plate.cells.length; i<length; i++) {
 		var cell = plate.cells[i];
-		var height = 0;
+		var height_rank = 0;
 		for (var j = 0, lj = zDotMultipliers.length; j < lj; j++) {
 			var z = cell.pos.clone().dot(zDotMultipliers[j]);
-			height += heaviside_approximation(z, 15);
+			height_rank += heaviside_approximation(z, 15);
 		};
-		console.log(height)
-		if(height > 0){
+		cell.height_rank = height_rank;
+	}
+
+	// order cells by this new found  "height rank"
+	cells = plate.cells
+		.slice(0)
+		.sort(function(a, b) { return a.height_rank - b.height_rank });
+
+	// 70% of earth is covered by water
+	// we'll use this fact until we can flesh out the rest of the algorithm.
+
+	for (var i = 0, li = cells.length; i < li; i++) {
+		var cell = cells[i];
+		if(i < li * 0.25){
 			cell.create(world.land);
 		} else {
 			cell.create(world.ocean);
 		}
 
 		cell.content.isostasy();
-	}
+	};
+
+	// Next we find elevations whose magnitude and frequency match those of earth's.
+	// To do this, we generate a second dataset of equal size that represents actual elevations.
+	// This dataset is generated from statistical distributions matching those found on earth. 
+	// We sort the elevations and map each one to a cell from our height-rank sorted list.
+
+	// Our model does not work directly with elevation.
+	// We must express elevation in terms of thickness/density
+	// To do this, we start off with a set of rock column templates expressing
+	// what thickness/density should look like at a given density.
+	// We then use B-splines to interpolate between these templated values.
+
 	plate.densityOffset = plate.getDensityOffset();
 	world.plates = [plate];
 }
