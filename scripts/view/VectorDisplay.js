8c89c46157b82919dac507d78fca938f51c70161
Carl Davidson <davids14@illinoisalumni.org>
2016-09-09 23:25:57 -0500

    vector display for velocity and angular velocity

--- scripts/view/VectorDisplay.js
+++ scripts/view/VectorDisplay.js
@@ -8,26 +8,50 @@ function VectorDisplay(options) {
 	var max = options['max'] || '1.';
 	this.getField = options['getField'];
 }
-VectorDisplay.prototype.addTo = function(mesh) {
-
-};
-VectorDisplay.prototype.removeFrom = function(mesh) {
-		
-};
+VectorDisplay.prototype.addTo = function(mesh) {};
+VectorDisplay.prototype.removeFrom = function(mesh) {};
 VectorDisplay.prototype.updateAttributes = function(material, plate) {
 	var material, displacement, scalar;
 	var vector = material.attributes.vector.value;
 	var is_member_model = plate.is_member; 
 	var vector_model = this.getField !== void 0? this.getField(plate) : new THREE.Vector3(0.03,0.03,0.03);
-	var is_member;
 	for(var i=0, li = is_member_model.length; i<li; i++){
 		var vector_i = 2*i+1;
-		is_member = is_member_model[i];
-		vector[vector_i] = is_member === 1?  new THREE.Vector3(1.00,0.00,0.00) : new THREE.Vector3(0.00,0.00,0.00); 
+		vector[vector_i] = is_member_model[i] === 1?  
+			plate.eulerPole.clone().multiplyScalar(1/30) : 
+			new THREE.Vector3(0.00,0.00,0.00); 
 	}
 	material.attributes.vector.needsUpdate = true;
 }
-vectorDisplays.test	= new VectorDisplay( {  } );
+vectorDisplays.angularVelocity	= new VectorDisplay( {  } );
+
+
+function VectorDisplay(options) {
+	var min = options['min'] || '0.';
+	var max = options['max'] || '1.';
+	this.getField = options['getField'];
+}
+VectorDisplay.prototype.addTo = function(mesh) {};
+VectorDisplay.prototype.removeFrom = function(mesh) {};
+VectorDisplay.prototype.updateAttributes = function(material, plate) {
+	var material, displacement, scalar;
+	var vector = material.attributes.vector.value;
+	var is_member_model = plate.is_member; 
+	var positions_model = plate.pos;
+	var vector_model = this.getField !== void 0? this.getField(plate) : new THREE.Vector3(0.03,0.03,0.03);
+	for(var i=0, li = is_member_model.length; i<li; i++){
+		var vector_i = 2*i+1;
+		vector[vector_i] = is_member_model[i] === 1?  
+			plate.eulerPole
+				.clone()
+				.cross(positions_model[i])
+				.normalize()
+				.multiplyScalar(0.17) : 
+			new THREE.Vector3(0.00,0.00,0.00); 
+	}
+	material.attributes.vector.needsUpdate = true;
+}
+vectorDisplays.velocity	= new VectorDisplay( {  } );
 
 
 function DisabledVectorDisplay(options) {}
