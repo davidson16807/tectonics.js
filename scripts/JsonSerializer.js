1b40bda36eacad2e72e5133254c18938f761528b
Carl Davidson <davids14@illinoisalumni.org>
2017-09-20 20:21:02 -0500

    cleaning JsonSerializer

--- scripts/JsonSerializer.js
+++ scripts/JsonSerializer.js
@@ -1,18 +1,3 @@
-function _abTostr(buf) {
-  return String.fromCharCode.apply(null, new Uint16Array(buf));
-}
-
-function _strToab(str) {
-  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-  var bufView = new Uint16Array(buf);
-  for (var i=0, strLen=str.length; i<strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-  return buf;
-}
-
-
-
 var JsonSerializer 	= {};
 JsonSerializer.world = function (world, options) {
 	options = options || {};
