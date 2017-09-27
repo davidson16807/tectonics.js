2bdc695420bb8b8932d7565cd0d19a7f533d6280
Carl Davidson <diadophus@gmail.com>
2017-09-27 15:02:21 -0500

    util functions for logging within main loops

--- scripts/Utils.js
+++ scripts/Utils.js
@@ -3,3 +3,23 @@
 function stop(message) {
 	throw message;
 }
+
+var log_once = (function () {
+	var logged = {};
+	return function(message) {
+		if (!logged[message]) {
+			logged[message] = true;
+			console.log(message);
+		}
+	}
+})();
+
+var throw_once = (function () {
+	var thrown = {};
+	return function(message) {
+		if (!thrown[message]) {
+			thrown[message] = true;
+			throw message;
+		}
+	}
+})();
