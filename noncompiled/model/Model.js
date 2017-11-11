2b29892383a945fa09c4568c2eccfcbf4487da5b
Carl Davidson <davids14@illinoisalumni.org>
2017-11-10 23:57:16 -0600

    removing Publisher
    It was an evolutionary dead end, created mostly to lead the way to parallel processing. Parallel processing doesn't fit anymore with the current raster based computational paradigm

--- noncompiled/model/Model.js
+++ noncompiled/model/Model.js
@@ -11,20 +11,7 @@ Model.prototype.world = function(world) {
 	if (world === void 0) {
 		return this._world;
 	};
-	console.log('called')
-	if(this._world !== void 0){
-		console.log('publishing model.world.remove')
-		Publisher.publish('crust', 'remove', { 
-			value: this._world, 
-			uuid: this._world.uuid } 
-		);
-	};
-	console.log('publishing model.world.add')
 	this._world = world;
-	Publisher.publish('crust', 'add', { 
-		value: world, 
-		uuid: world.uuid } 
-	);
 };
 
 Model.prototype.update = function(timestep) {
