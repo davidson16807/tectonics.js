1caeec970d5edc05239b1f96490b6961aa8e4b13
Carl Davidson <carl.davidson@pioneer.com>
2018-04-20 13:22:05 -0500

    uh, readding Orbit class

---
+++ noncompiled/model/orbit/Orbit.js
@@ -0,0 +1,72 @@
+'use strict';
+
+function Orbit(parameters) {
+
+	// private variables
+	var grid = parameters['grid'] || stop('missing parameter: "grid"');
+	var sample_num = parameters['orbit_sample_num'] || 12;
+
+	var incident_radiation_fraction = new Memo( //TODO: move this to a new "Orbit" class
+		Float32Raster(grid),  
+		function (result) {
+			return OrbitalMechanics.incident_radiation_fraction (
+				grid.pos, 
+				// this is a single vector of the planet's position in heliocentric eliptic coordinates (not to be confused with "pos")
+				orbital_pos.value(), 
+				axial_tilt, 
+				sample_num,
+				result
+			);
+		}
+	);
+
+	// public variables
+	var self = this;
+	this.daily_averaging = false;
+	this.monthly_averaging = false;
+	this.yearly_averaging = false;
+	this.milankovich_cycle_averaging = false;
+	this.global_solar_constant = parameters['global_solar_constant'] || 1.361; // kiloWatts per m^2 
+	this.mean_anomaly 	= parameters['mean_anomaly'] 	|| 0;
+	this.axial_tilt 	= parameters['axial_tilt'] 		|| Math.PI * 23.5/180;
+	this.angular_speed 	= parameters['angular_speed'] 	|| 460; // m/s
+	this.orbital_pos = new Memo( [], 
+		function (result) {
+			return OrbitalMechanics.get_eliptic_coordinate_sample(1, 0, world.mean_anomaly);
+		}
+	); 
+	this.incident_radiation = new Memo( //TODO: move this to a new "Orbit" class
+		Float32Raster(grid),  
+		function (result) {
+			return ScalarField.mult_scalar(self.incident_radiation_fraction.value(), self.global_solar_constant, result);
+		}
+	);
+
+	function assert_dependencies() { }
+
+	this.setDependencies = function(dependencies) { };
+
+	this.initialize = function() {
+		assert_dependencies();
+	}
+
+	this.invalidate = function() {
+		// NOTE: we don't need to invalidate these commented-out attributes, because the underlying data doesn't change often
+		// incident_radiation_fraction 		.invalidate(); 
+		// global_solar_constant 			.invalidate(); // TODO: uncomment this once you have a working model for stellar evolution
+		// incident_radiation 				.invalidate(); // TODO: uncomment this once you have a working model for stellar evolution
+		orbital_pos							.invalidate();
+	}
+
+	this.calcChanges = function(timestep) {
+		assert_dependencies();
+	};
+
+	this.applyChanges = function(timestep){
+		if (timestep === 0) {
+			return;
+		};
+		
+		assert_dependencies();
+	};
+}
