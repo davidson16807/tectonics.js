b2e0a93f112a34e00317fe1fc868f469e8a5c8cb
Carl Davidson <carl.davidson@pioneer.com>
2018-05-14 10:27:08 -0500

    creating the universe

--- noncompiled/model/universe/Universe.js
+++ noncompiled/model/universe/Universe.js
@@ -4,14 +4,22 @@
 // the data structure is designed to allow for:
 //  * iterative physics simulation
 //  * "on-rails" simulation, ala Kerbal Space Program
-//  * arbitrary assignment of cycle states from the user
+//  * arbitrary assignment of cycle states from the user (since any state is likely occur within a single million year timestep)
 // design principles:
-//  * must be able to easily traverse through and update the state of all spins and orbits
-//  * must be able to easily traverse through and update the state of all bodies
-//  * must be able to easily track the position and orientation of all bodies
-//  * must be able to easily find the incident radiation incurred on all bodies from all stars
-//
-//
+//  1. must be able to easily update the state of all "on-rails" motions
+//  2. must be able to easily update the state of all bodies
+//  3. must be able to easily track the position and orientation of all bodies relative to any other body
+//  4. must be able to easily find the incident radiation incurred on all bodies from all stars
+//  4. must be able to easily find the gravity induced on all bodies from all other bodies
+//  5. must be able to enumerate all bodies to for easy selection by the user
+// conclusions:
+//  a. must contain a nested structure of all bodies, since this reflects the underlying data
+//  b. from 1.), must contain an easily traversed structure of all motions
+//  c. from 2.), must contain an easily traversed structure of all bodies
+//  d. from 3.), must contain a function return a map between all bodies and their position/rotation matrices
+//  e. from 4.), must contain an easily traversed structure of all stars
+//  f. in order to accomplih 5.) using the data structures for a.), d.), and e.), the data structures must be index by a key
+//  
 // [
 // 	{
 // 		type: 'system'
@@ -22,7 +30,6 @@
 // 				axial_tilt: ...
 // 				body: sun
 // 			},
-
 // 			{
 // 				type: 'system'
 // 				sma: 1,
@@ -39,30 +46,71 @@
 //
 //
 //
-function Universe(parameters) {
+function Universe(motion_hierarchy) {
 	var self = this;
 
-	this.stars = parameters['stars'] | [];
-	this.bodies = {};
-	this.body_absolute_positions = {};
+	var motion_hierarchy = motion_hierarchy;
+	
+	var motion_hierarchy_nodes = motion_hierarchy.descendants();
 
-	this.orbits = {};
-	this.orbit_relative_positions = {};
+	var body_nodes = 
+		this.motion_hierarchy_nodes
+			.filter	(node => node.body)
+			// .map	(node => node.body)
 
-	this.spins = [];
+	var body_nodes_lookup = 
+		this.motion_hierarchy_nodes
+			.filter	(node => node.body)
+			.reduce	((map, node) => ({ ...map, [node.body.name]: node.body }), {}); // convert to dict
+			// .map	(node => node.body)
+
+	var star_nodes = 
+		this.motion_hierarchy_nodes
+			.filter	(node => node.body)
+			.filter	(node => node.body instanceof Star)
+			.map	(node => node.body)
+
+//	this.invalidate = function() {
+//
+//	}
+//
+//	this.setDependencies = function(dependencies) {
+//
+//	}
+//
+//	this.initialize = function() {
+//
+//	}
+
+	this.calcChanges = function(timestep) {
+		if (timestep === 0) {
+			return;
+		};
+
+		for (var i = 0; i < motion_hierarchy_nodes.length; i++) {
+			motion_hierarchy_nodes[i].calcChanges(timestep);
+		}
+		for (var i = 0; i < body_nodes.length; i++) {
+			body_nodes[i].body.calcChanges(timestep);
+		}
+	};
+
+	this.applyChanges = function(timestep) {
+		if (timestep === 0) {
+			return;
+		};
+
+		for (var i = 0; i < motion_hierarchy_nodes.length; i++) {
+			motion_hierarchy_nodes[i].applyChanges(timestep);
+		}
+		for (var i = 0; i < body_nodes.length; i++) {
+			body_nodes[i].body.applyChanges(timestep);
+		}
+	};
 
     // This calculates the incident radiation received by a celestial_body from all the light sources in the system
 	this.incident_radiation = function (celestial_body, result) { 
 		var grid = celestial_body.grid;
-
-		body = celestial_body;
-		var celestial_lineage = [body];
-		while (body.orbit.parent){
-			body = body.orbit.parent;
-			celestial_lineage.push(body);
-		}
-
-		var celestial_cycles = celestial_lineage.map( body => body.orbit.cycle())
 
 		// TODO: need scratch for these 
 		var surface_normal = VectorRaster(grid); 
@@ -111,27 +159,4 @@ function Universe(parameters) {
 		ScalarField.div_scalar(incident_radiation_sum, sample_num, result); 
 		return result; 
 	}
-
-	function assert_dependencies() { }
-
-	this.setDependencies = function(dependencies) { };
-
-	this.initialize = function() {
-		assert_dependencies();
-	}
-
-	this.invalidate = function() {
-	}
-
-	this.calcChanges = function(timestep) {
-		assert_dependencies();
-	};
-
-	this.applyChanges = function(timestep){
-		if (timestep === 0) {
-			return;
-		};
-		
-		assert_dependencies();
-	};
 }
