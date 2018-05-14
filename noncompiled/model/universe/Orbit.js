5c009bb8722d906129596b5f72ce783b7feb1d2f
Carl Davidson <carl.davidson@pioneer.com>
2018-05-14 10:26:18 -0500

    privatizing Orbit and Spin attributes and adding calc/applyChanges()

--- noncompiled/model/universe/Orbit.js
+++ noncompiled/model/universe/Orbit.js
@@ -2,8 +2,7 @@
 
 // An orbit is a low level mathematical data structure, similar in nature to the "Raster" classes.
 // It can be thought of as any conic path drawn across space, regardless of the force that causes it.
-// Along with the standard gravitational parameter, "GM" it can be treated as a unique tuple of position and velocity
-// this allows the calculation of time related parameters like velocity and period.
+// Along with the standard gravitational parameter, "GM" it can be treated as a function mapping time to a tuple of position and velocity
 // For a non-binary system, GM is equal to the gravitational constant multiplied by the mass of the parent object
 //  * An orbit does not require a planet - it could, for instance, describe a motion between barycenters.
 //    It is no more dependant on a celestial body than a velocity vector is dependant on a physical object
@@ -11,48 +10,73 @@
 //  * An orbit could be affected by celestial bodies over time. 
 //    Since this requires knowledge of all celestial objects in the universe, 
 //    We only handle this logic in the "Universe" class. 
-//  * "GM" is a constant derived from the mass of the parent object. 
-//    It is not typically included in textbooks amongst orbital parameters, 
-//    so we do not included it as an attribute of the "Orbit" class. 
 function Orbit(parameters) {
 	var self = this;
 
 	// public variables
 	// the phase angle (in radians) that indicates the state within an object's revolution. It varies linearly with time.
-	this.mean_anomaly					  = parameters['mean_anomaly'] 						|| 0;
+	var mean_anomaly					= parameters['mean_anomaly'] 						|| 0.;
 	// the average between apoapsis and periapsis
-	this.semi_major_axis				  = parameters['semi_major_axis'] 					|| stop('missing parameter: "semi_major_axis"');
+	var semi_major_axis					= parameters['semi_major_axis'] 					|| stop('missing parameter: "semi_major_axis"');
 	// the shape of the orbit, where 0 is a circular orbit and >1 is a hyperbolic orbit
-	this.eccentricity					  = parameters['eccentricity'] 						|| 0;
+	var eccentricity					= parameters['eccentricity'] 						|| 0.;
 	// the angle (in radians) between the orbital plane and the reference plane (the intersection being known as the "ascending node")
-	this.inclination					  = parameters['inclination'] 						|| 0;
+	var inclination						= parameters['inclination'] 						|| 0.;
 	// the angle (in radians) between the ascending node and the periapsis
-	this.argument_of_periapsis			  = parameters['argument_of_periapsis'] 			|| 0;
+	var argument_of_periapsis			= parameters['argument_of_periapsis'] 			|| 0.;
 	// the angle (in radians) between the prime meridian of the parent and the "ascending node" - the intersection between the orbital plane and the reference plane
-	this.longitude_of_ascending_node	  = parameters['longitude_of_ascending_node'] 		|| 0;
-}
-Orbit.fromStateVectors = function(position, velocity, result) {
-	// body...
-}
-// "position" returns the position vector that is represented by an orbit 
-Orbit.position = function(orbit) {
-	return OrbitalMechanics.get_self_centric_to_parent_centric_offset(
-		orbit.mean_anomaly,
-		orbit.semi_major_axis, 
-		orbit.eccentricity, 
-		orbit.inclination,
-		orbit.argument_of_periapsis,
-		orbit.longitude_of_ascending_node
-	);
-}
-// "period" returns the time required to complete one revolution of an orbit
-Orbit.period = function(orbit, GM) {
-	var a = orbit.semi_major_axis;
-	var π = Math.PI;
-	return 2*π * Math.sqrt( a*a*a / GM );
-}
-// "iterate" is a function which, given an orbit and a timestep, 
-// returns a new orbit representing the state of an object if left undisturbed
-Orbit.iterate = function(orbit, GM, timestep, result) {
-	throw "NOT IMPLEMENTED"
+	var longitude_of_ascending_node		= parameters['longitude_of_ascending_node'] 		|| 0.;
+	// effective mass of the parent body
+	// We say it is "effective" because sometimes no parent body exists (i.e. barycenters)
+	// It is not typically included in textbooks amongst orbital parameters,
+	// but it allows us to make statements that concern timing: velocity, period, etc.
+	var effective_parent_mass  			= parameters['effective_parent_mass'] 			|| 0.;
+
+	this.is_cycle = true;
+
+	this.period = function(orbit) {
+		return OrbitalMechanics.get_period(orbit.semi_major_axis, orbit.effective_parent_mass);
+	}
+
+	// "position" returns the position vector that is represented by an orbit 
+	this.get_child_to_parent_matrix = function(orbit) {
+		return Orbit.get_orbit_matrix4x4(
+				mean_anomaly,
+				semi_major_axis, 
+				eccentricity, 
+				inclination,
+				argument_of_periapsis,
+				longitude_of_ascending_node
+			);
+	}
+	this.get_parent_to_child_matrix = function() {
+		return Matrix4x4.invert(this.get_child_to_parent_matrix());
+	}
+
+	// private variables 
+	var mean_anomaly_refresh			  = 0.;
+
+	this.calcChanges = function(timestep) {
+		timestep *= Units.SECONDS_IN_MILLION_YEARS;
+		if (timestep === 0) {
+			return;
+		};
+		if (timestep/period > 1/10){
+			return;
+		}
+		var period = this.period();
+		mean_anomaly_refresh = (mean_anomaly + (timestep / period)) % period;
+	};
+
+	this.applyChanges = function(timestep) {
+		timestep *= Units.SECONDS_IN_MILLION_YEARS;
+		if (timestep === 0) {
+			return;
+		};
+		if (timestep/period > 1/10){
+			return;
+		}
+		mean_anomaly = mean_anomaly_refresh;
+	};
+	
 }
--- noncompiled/model/universe/Spin.js
+++ noncompiled/model/universe/Spin.js
@@ -3,21 +3,49 @@ function Spin(parameters) {
 	var self = this;
 
 	// public variables
-	this.precession_angle			= parameters['precession_angle'] 	|| 0;
-	this.axial_tilt 				= parameters['axial_tilt'] 			|| Math.PI * 23.5/180;
-	this.angular_speed 				= parameters['angular_speed'] 		|| 2*Math.PI/(60*60*24); // radians/s
-	this.rotation_angle 			= parameters['rotation_angle'] 		|| 0; // radians/s
-}
+	var precession_angle	= parameters['precession_angle'] 	|| 0;
+	var axial_tilt 			= parameters['axial_tilt'] 			|| Math.PI * 23.5/180;
+	var angular_speed 		= parameters['angular_speed'] 		|| 2*Math.PI/(60*60*24); // radians/s
+	var rotation_angle 		= parameters['rotation_angle'] 		|| 0; // radians/s
 
-Spin.rotation_matrix = function(rotation_angle) {
-	return OrbitalMechanics.get_equatorial_to_ecliptic_matrix(
-		rotation_angle, 
-		axial_tilt, 
-		precession_angle
-	);
-}
-// "iterate" is a function which, given an spin and a timestep, 
-// returns a new spin representing the state of an object if left undisturbed
-Spin.iterate = function(orbit, timestep, spin) {
-	
+	this.period = function(orbit) {
+		return 2/angular_speed;
+	}
+
+	this.get_child_to_parent_matrix = function() {
+		return OrbitalMechanics.get_spin_matrix4x4(
+			rotation_angle, 
+			axial_tilt, 
+			precession_angle
+		);
+	}
+	this.get_parent_to_child_matrix = function() {
+		return Matrix4x4.invert(this.get_child_to_parent_matrix());
+	}
+
+	// private variables 
+	var rotation_angle_refresh 		= 0.
+
+	this.calcChanges = function(timestep) {
+		timestep *= Units.SECONDS_IN_MILLION_YEARS;
+		if (timestep === 0) {
+			return;
+		};
+		if (timestep/period > 1/10){
+			return;
+		}
+		rotation_angle_refresh = (rotation_angle + (timestep / period)) % period;
+	};
+
+	this.applyChanges = function(timestep) {
+		timestep *= Units.SECONDS_IN_MILLION_YEARS;
+		if (timestep === 0) {
+			return;
+		};
+		if (timestep/period > 1/10){
+			return;
+		}
+		rotation_angle = rotation_angle_refresh;
+	};
+
 }
