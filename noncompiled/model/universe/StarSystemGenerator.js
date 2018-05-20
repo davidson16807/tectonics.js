0e83f1d11d98bde7c4bf42f0202c20b384d897dc
Carl Davidson <carl.davidson@pioneer.com>
2018-05-19 20:57:25 -0500

    skeleton for random star system generation

---
+++ noncompiled/model/universe/StarSystemGenerator.js
@@ -0,0 +1,30 @@
+// Considerations:
+//   anthropic principle (model need only generate habitable stars)
+//   star type frequency (Stellar Initial Mass Function)
+//   star metallicity (affects frequency of gas giants)
+//   conservation of angular momentum (for binary stars)
+//   planetary mass distributions
+//   Titus-Bode law / Dermott's law
+//   hill spheres
+//   statistical mechanics
+//
+// Outline:
+//   What is the combined mass of the system?
+//   how many stars (function of combined mass)
+//   what is the mass of each star?
+//   what is the period of binary stars? (log normal distribution, expected value of 100 years)
+//   what is the eccentricity of binary stars? (function of period)
+//   what is the orbital plane of stars relative to galaxy? (assume uniform distrubtion of rotation vectors/coord basis)
+//   ...
+//   
+// Notes:
+//   1/3 of binary stars have planets
+// 
+// 
+// 
+// 
+// 
+var StarSystemGenerator = {};
+StarSystemGenerator.generate = function () {
+
+}
