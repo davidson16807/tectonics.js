813a2e6c9cf13ee6eb10d9dda6868fb98b03216f
Carl Davidson <carl.davidson@pioneer.com>
2018-05-21 15:36:26 -0500

    dropping precession from Spin, adding earth precession as separate spin

--- noncompiled/model/universe/Spin.js
+++ noncompiled/model/universe/Spin.js
@@ -3,19 +3,17 @@ function Spin(parameters) {
 	var self = this;
 
 	// public variables
-	var precession_angle	= parameters['precession_angle'] 	|| 0;
-	var axial_tilt 			= parameters['axial_tilt'] 			|| Math.PI * 23.5/180;
+	var axial_tilt 			= parameters['axial_tilt'] 			|| 0;
 	var angular_speed 		= parameters['angular_speed'] 		|| 2*Math.PI/(60*60*24); // radians/s
 
 	this.period = function(orbit) {
-		return 2/angular_speed;
+		return 2*Math.PI/angular_speed;
 	}
 
 	this.get_child_to_parent_matrix = function(rotation_angle) {
 		return OrbitalMechanics.get_spin_matrix4x4(
 			rotation_angle, 
-			axial_tilt, 
-			precession_angle
+			axial_tilt
 		);
 	}
 	this.get_parent_to_child_matrix = function(rotation_angle) {
