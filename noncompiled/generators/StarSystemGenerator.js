2bfe644bdc26ea05c79cd3a749d5ce6ee3d84b50
Carl Davidson <davids14@illinoisalumni.org>
2018-05-21 08:29:39 -0500

    moving generator

---
+++ noncompiled/generators/StarSystemGenerator.js
@@ -0,0 +1,105 @@
+// Considerations:
+//   anthropic principle (model need only generate habitable stars, at least for now)
+//   star type frequency (Stellar Initial Mass Function)
+//   star metallicity (affects frequency of gas giants)
+//   conservation of angular momentum (for binary stars)
+//   planetary mass distributions
+//   Titus-Bode law / Dermott's law
+//   hill spheres
+//   statistical mechanics
+//
+// Outline:
+//   What is the combined mass of the system?
+//   how many stars (function of combined mass)
+//   what is the mass of each star?
+//   what is the period of binary stars? (log normal distribution, expected value of 100 years)
+//   what is the eccentricity of binary stars? (function of period)
+//   what is the orbital plane of stars relative to galaxy? (assume uniform distrubtion of rotation vectors/coord basis)
+//   type of system: p type, s type, frost line, hot jupiter, adopted planets
+//   planetary periods, semi-major axes, and eccentricities
+//   original semi-major axes at which they formed
+//   time of stellar ignition
+//   composition (determined by stellar distance and time of stellar ignition: ice giants formed after ignition)
+//   ...
+//   
+// 
+// 
+// 
+// 
+// 
+var StarSystemGenerator = {};
+StarSystemGenerator.generate = function (random, system_mass) {
+
+	// Returns a function that generates a weighted random choice 
+	// Weights are indicated by a dict representing a stat distribution
+	// TODO: put this to its own namespace
+	// we use it again under NameGenerator
+	random_weighted_choice = function(distribution) {
+	  var i, j, table=[];
+	  for (i in distribution) {
+	    // The constant 10 below should be computed based on the
+	    // weights in the distribution for a correct and optimal table size.
+	    // E.g. the distribution {0:0.999, 1:0.001} will break this impl.
+	    for (j=0; j<distribution[i]*10; j++) {
+	      table.push(i);
+	    }
+	  }
+	  return function() {
+	    return table[Math.floor(Math.random() * table.length)];
+	  }
+	}
+
+	var log = Math.log;
+	var exp = Math.exp;
+
+	// See Chabrier 2003
+	system_mass = system_mass || 
+		Units.SOLAR_MASS * exp(random.normal(log(0.2), 0.6));
+
+	// system_ref_frame = 
+
+	// semi major axis of binary system's galactic orbit 
+	var galactic_sma = 
+
+	// 1/3 of binary stars have planets
+	if (random.uniform(0,1) < 1/3){
+		// mass of subsystem1 vs mass of other star
+		var system_mass_ratio = exp(random.normal(log(0.2), 0.6)) / exp(random.normal(log(0.2), 0.6)) 
+		// mass of each subsystem vs total system mass
+		var subsystem1_mass_fraction = system_mass_ratio/(system_mass_ratio+1);
+		var subsystem2_mass_fraction = 1.-subsystem1_mass_fraction;
+		// mass of each subsystem
+		var subsystem1 = StarSystemGenerator.generate(random, system_mass*subsystem1_mass_fraction);
+		var subsystem2 = StarSystemGenerator.generate(random, system_mass*subsystem2_mass_fraction);
+
+		// eccentricity of subsystems orbiting themselves
+		var eccentricity = random.uniform(0,1) // TODO: model this as a beta distribution
+		// semi major axis of subsystem1, from Malkov et al.
+		var subsystem1_sma = Units.ASTRONOMICAL_UNIT * exp(random.normal(log(12), log(2))); 
+		// semi major axis of subsystem2, given conservation of momentum/center of mass
+		var subsystem2_sma = subsystem1_sma / system_mass_ratio;
+
+
+		return new System({
+			// TODO: galactic orbit
+			motion: new Orbit({
+				semi_major_axis: galactic_sma,
+			}),
+			children: [
+				new System({
+					motion: new Orbit({
+						semi_major_axis: subsystem1_sma,
+					}),
+					children: [subsystem1],
+				}),
+
+				new System({
+					motion: new Orbit({
+						semi_major_axis: subsystem2_sma,
+					}),
+					children: [subsystem2],
+				}),
+			],
+		})
+	}
+}
