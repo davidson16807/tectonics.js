b65b17215cc75cc7acfa955804aa130fcea3ac4d
Carl Davidson <carl.davidson@pioneer.com>
2018-06-12 16:44:41 -0500

    fixes, and refactorings - still untested

--- noncompiled/model/universe/Universe.js
+++ noncompiled/model/universe/Universe.js
@@ -52,29 +52,27 @@
 function Universe(hierarchy, config) {
 	config = config || {};
 
-	var id_to_node_map = this
-		.descendants()
+	var nodes = hierarchy.descendants();
+	var id_to_node_map = nodes
 		.reduce((acc, x) => { acc[x.name] = x; return acc; }, {} );
-	var nodes_by_period = this
-		.descendants()
+	var nodes_by_period = nodes
 		.sort((a,b) => a.motion.period() - b.motion.period())
 		.reverse();
-	var bodies = this
-		.descendants()
+	var bodies = nodes
 		.filter(x => x.body !== void 0)
 		.map(x => x.body);
-	var body_id_to_node_map = this
-		.descendants()
+	var body_id_to_node_map = nodes
 		.filter(x => x.body !== void 0)
 		.reduce((acc, x) => { acc[x.body.name] = x; return acc; }, {} );
 
+	// time below which user could no longer perceive the effects of a cycle, in simulated seconds
+	// NOTE: min_perceivable_period = 30/2 * timestep // half a second of real time
+	// time above which user could no longer perceive the effects of a cycle, in simulated seconds
+	// NOTE: max_perceivable_period = 60*60*24*30 * timestep // 1 day worth of real time at 30fps
+
 	//given a cycle configuration, "advance()" returns the cycle configuration that would occur after a given amount of time
-	function advance(config, timestep, output, min_frames_per_cycle, max_frames_per_cycle) {
+	function advance(config, output, min_perceivable_period, max_perceivable_period) {
 		output = output || {};
-		// number of frames below which user could no longer perceive the effects of a cycle, in seconds
-		min_frames_per_cycle = min_frames_per_cycle || 30/2; // half a second
-		// number of frames above which user could no longer perceive the effects of a cycle, in seconds
-		max_frames_per_cycle = max_frames_per_cycle || 60*60*24*30; // 1 day worth at 30fps
 
 		for(id in id_to_node_map){
 			if (id_to_node_map[id] === void 0) { continue; }
@@ -82,9 +80,9 @@ function Universe(hierarchy, config) {
 			// default to current value, if present
 			if (config[id]) { output[id] = config[id]};
 			// if cycle completes too fast for the user to perceive, don't simulate 
-			if (period / timestep > min_frames_per_cycle ) 	{ continue; }
+			if (period < min_perceivable_period) 	{ continue; }
 			// if cycle completes too slow for the user to perceive, don't simulate
-			if (period / timestep < max_frames_per_cycle ) 	{ continue; }
+			if (period > max_perceivable_period) 	{ continue; }
 			output[id] = ((config[id] || 0) + 2*Math.PI * (timestep / period)) % (2*Math.PI);
 		}
 
@@ -95,25 +93,22 @@ function Universe(hierarchy, config) {
 	// "sample()" generates a list of cycle configurations that are representative of that timestep
 	// this is useful for finding, e.g. mean daily solar radiation
 	// the function will not generate more than a given number of samples per cycle
-	function samples(config, timestep, samples_per_cycle, min_frames_per_cycle) {
-		// number of frames below which user could no longer perceive the effects of a cycle, in seconds
-		min_frames_per_cycle = min_frames_per_cycle || 30/2; // half a second
-
+	function samples(config, samples_per_cycle, min_perceivable_period) {
 		// list of configs to sample across, starting with a clone of config
 		var samples = [Object.assign({}, config)];
 		// for each imperceptably small cycle:
 		for(node of nodes_by_period) {
 			if (node === void 0) { continue; }
 			var period = node.motion.period();
-			// if the cycle takes more than a given number of frames to complete, 
-			// then it can be perceived by the user, so don't sample across it
-			if (period / timestep > min_frames_per_cycle ) 	{ continue; }
+			// if the cycle takes more than a given amount to complete, 
+			// then it can be simulated, so don't sample across it
+			if (period > min_perceivable_period) 	{ continue; }
 			// sample across the cycle's period and add results to `samples`
 			var period = node.motion.period();
 			var subsamples = [];
 			for (sample of samples) {
 				for (var j = 0; j < samples_per_cycle; j++) {
-					subsamples.push(this.advance(sample, j*period/samples_per_cycle, {}, 1));
+					subsamples.push(advance(sample, j*period/samples_per_cycle, {}, 1));
 				}
 			}
 			samples = subsamples;
@@ -121,42 +116,56 @@ function Universe(hierarchy, config) {
 		return samples;
 	}
 
-	function insolation(config, body, insolation) {
-		var insolation = insolation || Float32Raster(body.grid);
-		var insolation_sample = Float32Raster(body.grid);
-
+	function insolation(config, body, star, insolation) {
 		var origin = body_id_to_node_map[body.name];
 		var surface_normal = body.grid.pos;
 
 		var body_matrices = origin.get_body_matrices(config);
 		// clear out result
-		Float32Raster.fill(insolation, 0);
-		var stars = bodies.filter(body => body instanceof Star);
-		for (star of stars) {
-			var star_matrix = body_matrices[star.name];
-			var star_pos = Matrix4x4.get_translation(star_matrix);
-			// calculate insolation effects of a single star
-			Optics.incident_radiation(
-				surface_normal,
-				star_pos, 
-				star.luminosity,
-				insolation_sample
-			);
-			// TODO: add effects of occlusion, e.g. moons around gas giants
-			ScalarField.add_field(insolation, insolation_sample, insolation);
-		}
+		var star_matrix = body_matrices[star.name];
+		var star_pos = Matrix4x4.get_translation(star_matrix);
+		// calculate insolation effects of a single star
+		Optics.incident_radiation(
+			surface_normal,
+			star_pos, 
+			star.luminosity,
+			insolation
+		);
+		// TODO: add effects of occlusion, e.g. moons around gas giants
 		return insolation;
 	}
-
-	this.average_insolation(body, timestep, samples_per_cycle, min_frames_per_cycle){
+	
+	// average insolation from a single star
+	average_insolation_from_star = function(body, star, min_perceivable_period, average_insolation, samples_per_cycle){
+		samples_per_cycle = samples_per_cycle || 6;
+		var average_insolation = average_insolation || Float32Raster(body.grid);
 		var insolation_sample = Float32Raster(body.grid);
+		var total_insolation = average_insolation; // double duty for performance
+		Float32Raster.fill(total_insolation, 0);
 
-		min_frames_per_cycle = 100;
-		var samples_ = samples(config, timestep, samples_per_cycle, min_frames_per_cycle);
+		var samples_ = samples(config, timestep, samples_per_cycle, min_perceivable_period);
 		for (sample of samples_){
-			insolation(sample, body, insolation_sample);
-			
+			insolation(sample, body, star, insolation_sample);
+			ScalarField.add_field(total_insolation, insolation_sample, total_insolation);
 		}
+		return ScalarField.div_scalar(total_insolation, samples_.length, average_insolation);
+	}
+
+	// average insolation from all stars
+	// TODO: memoize this into "average_insolation_from_star" and "reference_luminosity"
+	//   correct memoized result to reflect actual luminosity as star ages
+	this.average_insolation = function(body, min_perceivable_period, total_insolation, samples_per_cycle){
+		samples_per_cycle = samples_per_cycle || 6;
+		var insolation_sample = Float32Raster(body.grid);
+		var total_insolation = average_insolation; // double duty for performance
+		Float32Raster.fill(total_insolation, 0);
+
+		var stars = bodies.filter(body => body instanceof Star);
+		for (star of stars){
+			insolation(body, star, min_perceivable_period, insolation_sample, samples_per_cycle);
+			ScalarField.add_field(total_insolation, insolation_sample, total_insolation);
+		}
+		return total_insolation;
 	}
 
 //
