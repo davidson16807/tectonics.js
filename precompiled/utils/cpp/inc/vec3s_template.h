a90aeddad1b3dd45e1cd39b63eec5e86896b0180
Carl Davidson <carl.davidson@pioneer.com>
2018-10-10 14:12:37 -0500

    adding more intuitive behavior for vec3 comparison

--- precompiled/utils/cpp/inc/vec3s_template.h
+++ precompiled/utils/cpp/inc/vec3s_template.h
@@ -133,11 +133,148 @@ namespace Rasters
 
 
 
-
-
-
-
 		~vec3s_template() {};
+
+
+
+
+		static bool eq(const vec3s_template<T,N>& u, const T a, const T threshold)
+		{
+			bool out = true;
+			for (int i = 0; i < N; ++i)
+			{
+				out &= (u.values[i] - a).magnitude() < threshold;
+			}
+			return out;
+		}
+		static bool ne(const vec3s_template<T,N>& u, const T a, const T threshold)
+		{
+			bool out = false;
+			for (int i = 0; i < N; ++i)
+			{
+				out |= (u.values[i] - a).magnitude() > threshold;
+			}
+			return out;
+		}
+		static bool eq(const vec3s_template<T,N>& u, const vec1s_template<T,N>& a, const T threshold)
+		{
+			bool out = true;
+			for (int i = 0; i < N; ++i)
+			{
+				out &= (u.values[i] - a.values[i]).magnitude() < threshold;
+			}
+			return out;
+		}
+		static bool ne(const vec3s_template<T,N>& u, const vec1s_template<T,N>& a, const T threshold)
+		{
+			bool out = false;
+			for (int i = 0; i < N; ++i)
+			{
+				out |= (u.values[i] - a.values[i]).magnitude() > threshold;
+			}
+			return out;
+		}
+		static bool eq(const vec3s_template<T,N>& u, const vec3_template<T> v, const T threshold)
+		{
+			bool out = true;
+			for (int i = 0; i < N; ++i)
+			{
+				out &= (u.values[i] - v).magnitude() < threshold;
+			}
+			return out;
+		}
+		static bool ne(const vec3s_template<T,N>& u, const vec3_template<T> v, const T threshold)
+		{
+			bool out = false;
+			for (int i = 0; i < N; ++i)
+			{
+				out |= (u.values[i] - v).magnitude() > threshold;
+			}
+			return out;
+		}
+		static bool eq(const vec3s_template<T,N>& u, const vec3s_template<T,N>& v, const T threshold)
+		{
+			bool out = true;
+			for (int i = 0; i < N; ++i)
+			{
+				out &= (u.values[i] - v.values[i]).magnitude() < threshold;
+			}
+			return out;
+		}
+		static bool ne(const vec3s_template<T,N>& u, const vec3s_template<T,N>& v, const T threshold)
+		{
+			bool out = false;
+			for (int i = 0; i < N; ++i)
+			{
+				out |= (u.values[i] - v.values[i]).magnitude() > threshold;
+			}
+			return out;
+		}
+
+
+
+
+		static void eq(const vec3s_template<T,N>& u, const T a, vec1s_template<bool,N>& out, const T threshold)
+		{
+			for (int i = 0; i < N; ++i)
+			{
+				out.values[i] = (u.values[i] - a).magnitude() < threshold;
+			}
+		}
+		static void ne(const vec3s_template<T,N>& u, const T a, vec1s_template<bool,N>& out, const T threshold)
+		{
+			for (int i = 0; i < N; ++i)
+			{
+				out.values[i] = (u.values[i] - a).magnitude() > threshold;
+			}
+		}
+
+		static void eq(const vec3s_template<T,N>& u, const vec1s_template<T,N>& a, vec1s_template<bool,N>& out, const T threshold)
+		{
+			for (int i = 0; i < N; ++i)
+			{
+				out.values[i] = (u.values[i] - a.values[i]).magnitude() < threshold;
+			}
+		}
+		static void ne(const vec3s_template<T,N>& u, const vec1s_template<T,N>& a, vec1s_template<bool,N>& out, const T threshold)
+		{
+			for (int i = 0; i < N; ++i)
+			{
+				out.values[i] = (u.values[i] - a.values[i]).magnitude() > threshold;
+			}
+		}
+
+
+		static void eq(const vec3s_template<T,N>& u, const vec3_template<T> v, vec1s_template<bool,N>& out, const T threshold)
+		{
+			for (int i = 0; i < N; ++i)
+			{
+				out.values[i] = (u.values[i] - v).magnitude() < threshold;
+			}
+		}
+		static void ne(const vec3s_template<T,N>& u, const vec3_template<T> v, vec1s_template<bool,N>& out, const T threshold)
+		{
+			for (int i = 0; i < N; ++i)
+			{
+				out.values[i] = (u.values[i] - v).magnitude() > threshold;
+			}
+		}
+
+
+		static void eq(const vec3s_template<T,N>& u, const vec3s_template<T,N>& v, vec1s_template<bool,N>& out, const T threshold)
+		{
+			for (int i = 0; i < N; ++i)
+			{
+				out.values[i] = (u.values[i] - v.values[i]).magnitude() < threshold;
+			}
+		}
+		static void ne(const vec3s_template<T,N>& u, const vec3s_template<T,N>& v, vec1s_template<bool,N>& out, const T threshold)
+		{
+			for (int i = 0; i < N; ++i)
+			{
+				out.values[i] = (u.values[i] - v.values[i]).magnitude() > threshold;
+			}
+		}
 
 
 
@@ -495,6 +632,41 @@ namespace Rasters
 			}
 		}
 
+
+		bool operator==(const T a) const
+		{
+			return vec3s_template<T,N>::eq(*this, a);
+		}
+		bool operator!=(const T a) const
+		{
+			return vec3s_template<T,N>::ne(*this, a);
+		}
+		bool operator==(const vec1s_template<T,N> a) const
+		{
+			return vec3s_template<T,N>::eq(*this, a);
+		}
+		bool operator!=(const vec1s_template<T,N> a) const
+		{
+			return vec3s_template<T,N>::ne(*this, a);
+		}
+		bool operator==(const vec3_template<T> v) const
+		{
+			return vec3s_template<T,N>::eq(*this, v);
+		}
+		bool operator!=(const vec3_template<T> v) const
+		{
+			return vec3s_template<T,N>::ne(*this, v);
+		}
+		bool operator==(const vec3s_template<T,N>& v) const
+		{
+			return vec3s_template<T,N>::eq(*this, v);
+		}
+		bool operator!=(const vec3s_template<T,N>& v) const
+		{
+			return vec3s_template<T,N>::ne(*this, v);
+		}
+		
+
 		vec3s_template<bool,N> operator>(const T a) const
 		{
 			vec3s_template<bool,N> out = vec3s_template<T,N>();
@@ -519,18 +691,8 @@ namespace Rasters
 			vec3s_template<T,N>::lte(*this, a, out);
 			return out;
 		}
-		vec3s_template<bool,N> operator==(const T a) const
-		{
-			vec3s_template<bool,N> out = vec3s_template<T,N>();
-			vec3s_template<T,N>::eq(*this, a, out);
-			return out;
-		}
-		vec3s_template<bool,N> operator!=(const T a) const
-		{
-			vec3s_template<bool,N> out = vec3s_template<T,N>();
-			vec3s_template<T,N>::ne(*this, a, out);
-			return out;
-		}
+
+		
 
 		vec3s_template<bool,N> operator>(const vec1s_template<T,N> a) const
 		{
@@ -556,18 +718,8 @@ namespace Rasters
 			vec3s_template<T,N>::lte(*this, a, out);
 			return out;
 		}
-		vec3s_template<bool,N> operator==(const vec1s_template<T,N> a) const
-		{
-			vec3s_template<bool,N> out = vec3s_template<T,N>();
-			vec3s_template<T,N>::eq(*this, a, out);
-			return out;
-		}
-		vec3s_template<bool,N> operator!=(const vec1s_template<T,N> a) const
-		{
-			vec3s_template<bool,N> out = vec3s_template<T,N>();
-			vec3s_template<T,N>::ne(*this, a, out);
-			return out;
-		}
+		
+
 
 		vec3s_template<bool,N> operator>(const vec3_template<T> v) const
 		{
@@ -593,18 +745,8 @@ namespace Rasters
 			vec3s_template<T,N>::lte(*this, v, out);
 			return out;
 		}
-		vec3s_template<bool,N> operator==(const vec3_template<T> v) const
-		{
-			vec3s_template<bool,N> out = vec3s_template<T,N>();
-			vec3s_template<T,N>::eq(*this, v, out);
-			return out;
-		}
-		vec3s_template<bool,N> operator!=(const vec3_template<T> v) const
-		{
-			vec3s_template<bool,N> out = vec3s_template<T,N>();
-			vec3s_template<T,N>::ne(*this, v, out);
-			return out;
-		}
+
+		
 
 		vec3s_template<bool,N> operator>(const vec3s_template<T,N>& v) const
 		{
@@ -630,19 +772,6 @@ namespace Rasters
 			vec3s_template<T,N>::lte(*this, v, out);
 			return out;
 		}
-		vec3s_template<bool,N> operator==(const vec3s_template<T,N>& v) const
-		{
-			vec3s_template<bool,N> out = vec3s_template<T,N>();
-			vec3s_template<T,N>::eq(*this, v, out);
-			return out;
-		}
-		vec3s_template<bool,N> operator!=(const vec3s_template<T,N>& v) const
-		{
-			vec3s_template<bool,N> out = vec3s_template<T,N>();
-			vec3s_template<T,N>::ne(*this, v, out);
-			return out;
-		}
-
 
 
 
@@ -766,13 +895,4 @@ namespace Rasters
 	using ivec3s = vec3s_template<int, N>;
 	template <int N>
 	using bvec3s = vec3s_template<bool, N>;
-
-	template <class T, int N>
-	struct vec3_raster: vec3s_template<T,N>
-	{
-		//Grid* grid;
-		//mat4* frame;
-		vec3_raster(){};
-		~vec3_raster(){};
-	};
 }
