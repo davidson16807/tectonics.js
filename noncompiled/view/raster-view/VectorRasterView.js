8b5fae2efdd4152ce996cf6c742d688530a0c903
Carl Davidson <davids14@illinoisalumni.org>
2018-11-25 11:56:02 -0600

    VectorRasterView.upsert() now implemented similarly to other views

--- noncompiled/view/raster-view/VectorRasterView.js
+++ noncompiled/view/raster-view/VectorRasterView.js
@@ -2,40 +2,57 @@
 
 function VectorRasterView(options) {
 	this.max = options['max'];
+
+
 	this.mesh = void 0;
+	var mesh = void 0;
+	var vertexShader = void 0;
+
+	function create_mesh(raster, options) {
+		var grid = raster.grid;
+		var geometry = new THREE.Geometry();
+		for (var i=0, li=grid.vertices.length; i<li; ++i) {
+		    geometry.vertices.push( grid.vertices[i].clone() );
+		    geometry.vertices.push( grid.vertices[i].clone() );
+		}
+		var material = new THREE.ShaderMaterial({
+		        vertexShader: 	options.vertexShader,
+		        fragmentShader: fragmentShaders.vectorField,
+		        attributes: {
+		        },
+		        uniforms: { 
+			  		index: 		{ type: 'f', value: 0 }
+		        }
+		    });
+		return new THREE.Line( geometry, material, THREE.LinePieces);
+	}
+	function update_vertex_shader(value) {
+		if (vertexShader !== value) {
+			vertexShader = value;
+			mesh.material.vertexShader = value; 
+			mesh.material.needsUpdate = true; 
+		}
+	}
+
 	this.upsert = function(scene, raster, options) {
 
-		if (this.mesh === void 0) {
-			var grid = raster.grid;
-			var geometry = new THREE.Geometry();
-			for (var i=0, li=grid.vertices.length; i<li; ++i) {
-			    geometry.vertices.push( grid.vertices[i].clone() );
-			    geometry.vertices.push( grid.vertices[i].clone() );
-			}
-			var material = new THREE.ShaderMaterial({
-			        vertexShader: 	options.vertexShader,
-			        fragmentShader: fragmentShaders.vectorField,
-			        attributes: {
-			        },
-			        uniforms: { 
-				  		index: 		{ type: 'f', value: 0 }
-			        }
-			    });
-			var mesh = new THREE.Line( geometry, material, THREE.LinePieces);
+		if (mesh === void 0) {
+			mesh = create_mesh(raster, options);
+			vertexShader = options.vertexShader;
 			scene.add(mesh);
-			this.mesh = mesh;
-		}
 
-		var mesh = this.mesh;
-		var material = mesh.material;
-		var geometry = mesh.geometry;
+			// HACK: we expose mesh here so WorldViews can modify as they see fit, 
+			// 	  namely for displacement and sealevel attributes
+			this.mesh = mesh; 
+		} 
+
+		update_vertex_shader(options.vertexShader);
 
 		var offset_length = 1.02; 	// offset of arrow from surface of sphere, in radii
 		var max_arrow_length = 0.1; // max arrow length, in radii
-		var vector = geometry.vertices;
+		var vector = mesh.geometry.vertices;
 
 		var max = this.max ||  Math.max.apply(null, VectorField.magnitude(raster));
-			log_once(vector)
 		var scaling = max_arrow_length / max;
 
 		var pos = raster.grid.pos;
@@ -49,21 +66,13 @@ function VectorRasterView(options) {
 			end.y = raster.y[i] * scaling + start.y;
 			end.z = raster.z[i] * scaling + start.z;
 		}
-		// geometry.vertices.needsUpdate = true;
-		geometry.verticesNeedUpdate = true;
+		mesh.geometry.verticesNeedUpdate = true;
 	};
 	this.remove = function(scene) {
-		scene.remove(this.mesh);
-		this.mesh.geometry.dispose();
-		this.mesh.material.dispose();
-		this.mesh = undefined;
+		scene.remove(mesh);
+		mesh.geometry.dispose();
+		mesh.material.dispose();
+		mesh = void 0;
+		this.mesh = void 0;
 	};
-	this.vertexShader = function(vertexShader) {
-		this.mesh.material.vertexShader = vertexShader; 
-		this.mesh.material.needsUpdate = true; 
-	}
-	this.uniform = function(key, value) {
-		this.mesh.material.uniforms[key].value = value;
-		this.mesh.material.uniforms[key].needsUpdate = true;
-	}
 }
