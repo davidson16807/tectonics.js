48fc46aef1fcf8a7abe7a28cbce199c0750da48c
Carl Davidson <carl.davidson@pioneer.com>
2018-11-29 17:21:55 -0600

    more units

--- noncompiled/utils/Units.js
+++ noncompiled/utils/Units.js
@@ -1,18 +1,29 @@
 var Units = {};
 
-Units.TON = 1000.;							// kg
+Units.MICROGRAM = 1e-9;						// kilograms
+Units.MILLIGRAM = 1e-6;						// kilograms
+Units.GRAM = 1e-3;							// kilograms
+Units.KILOGRAM = 1;							// kilograms
+Units.TON = 1000.;							// kilograms
 
-Units.SOLAR_MASS = 2e30 					// kg
-Units.SOLAR_RADIUS = 695.7e6 				// meters
-Units.SOLAR_LUMINOSITY = 3.828e26 			// watts
+Units.MICROMETER = 1e-6;					// meter
+Units.MILLIMETER = 1e-3;					// meter
+Units.METER = 1;							// meter
+Units.KILOMETER = 1000;						// meter
 
-Units.JUPITER_MASS = 1.898e27; 				// kg
+Units.EARTH_MASS = 5.972e24; 				// kilograms
+Units.EARTH_RADIUS = 6.367e6;			 	// meters
+Units.STANDARD_GRAVITY = 9.80665; 			// meters/second^2
+Units.STANDARD_TEMPERATURE = 273.15; 		// kelvin
+Units.STANDARD_PRESSURE = 101325; 			// pascals
+Units.ASTRONOMICAL_UNIT = 149597870700; 	// meters
+
+Units.JUPITER_MASS = 1.898e27; 				// kilograms
 Units.JUPITER_RADIUS = 71e6; 				// meters
 
-Units.EARTH_MASS = 6367e3; 					// kg
-Units.EARTH_RADIUS = 6.367e6; 				// meters
-Units.STANDARD_GRAVITY = 9.80665; 			// meters/second^2
-Units.ASTRONOMICAL_UNIT = 149597870700; 	// meters
+Units.SOLAR_MASS = 2e30 					// kilograms
+Units.SOLAR_RADIUS = 695.7e6 				// meters
+Units.SOLAR_LUMINOSITY = 3.828e26 			// watts
 
 Units.SECOND = 1;							// seconds
 Units.MINUTE = 60;							// seconds
