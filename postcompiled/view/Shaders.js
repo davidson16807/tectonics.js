58936d4b1a23944ee025a0edf5618b358645cee3
Carl Davidson <davids14@illinoisalumni.org>
2018-11-28 21:06:48 -0600

    more polishing

--- postcompiled/view/Shaders.js
+++ postcompiled/view/Shaders.js
@@ -240,8 +240,7 @@ const float PI = 3.14159265358979;
 uniform float animation_phase_angle;
 varying float vVectorFractionTraversed;
 void main() {
- (cos(2.*PI*vVectorFractionTraversed - animation_phase_angle) + 1.) / 2.
- float state = gaussian(vVectorFractionTraversed - );
+ float state = (cos(2.*PI*vVectorFractionTraversed - animation_phase_angle) + 1.) / 2.;
  gl_FragColor = vec4(state) * vec4(vec3(0.8),0.) + vec4(vec3(0.2),0.);
 }
 `;
