87c650c901752c95ff67d3350ace5cd9e9d03566
Carl Davidson <carl.davidson@pioneer.com>
2019-01-16 18:22:31 -0600

    writing skeleton for main ray marching algorithm

--- postcompiled/Shaders.js
+++ postcompiled/Shaders.js
@@ -538,13 +538,63 @@ const vec3 betaM = vec3(21e-6); // Mie
 // thickness of the atmosphere if its density were uniform
 // we use a vec2: x is rayleigh scattering, y is mie scattering
 const vec2 scale_heights = vec2(7994, 1200);
-// maximum number of samples we alot ourselves for a single pixel
-// considers samples taken across all stars
-const int SAMPLE_BUDGET = 128;
-const int SAMPLE_COUNT = 16;
-const int SAMPLE_COUNT_LIGHT = 8;
-vec3 get_rgb_intensity_of_light_through_atmosphere(
+vec3 get_rgb_intensity_of_light_ray_through_atmosphere(
+ vec3 view_origin, vec3 view_direction,
+ vec3 world_position, float world_radius,
+ vec3 light_direction, vec3 light_rgb_intensity,
+ vec3 background_rgb_intensity,
+ vec2 atmosphere_scale_heights
 ){
+ // NOTE: 3 scale heights should capture ~95% of the atmosphere's mass, 
+ //   this should be enough to be aesthetically appealing.
+ float atmosphere_height = 3. * max(atmosphere_scale_heights.x, atmosphere_scale_heights.y);
+ float view_r_closest2; // distance ("radius") from the view ray to the center of the world at closest approach, squared;
+ float view_x_closest; // distance along the view ray at which closest approach occurs
+ float view_x_enter; // distance along the view ray at which the ray enters the atmosphere
+ float view_x_exit; // distance along the view ray at which the ray exits the atmosphere
+ const float VIEW_MARCH_STEP_COUNT = 16.;// number of steps taken while marching along the view ray
+ float view_march_dx; // distance between steps while marching along the view ray
+ float view_march_x; // distance traversed while marching along the view ray
+ float view_march_h; // distance ("height") from the surface of the world while marching along the view ray
+ vec3 view_march_pos; // absolute position while marching along the view ray
+ float light_r_closest2; // distance ("radius") from the light ray to the center of the world at closest approach, squared;
+ float light_x_closest; // distance along the light ray at which closest approach occurs
+ float light_x_enter; // distance along the light ray at which the ray enters the atmosphere, never used
+ float light_x_exit; // distance along the light ray at which the ray exits the atmosphere
+ const float LIGHT_MARCH_STEP_COUNT = 8.;// number of steps taken while marching along the light ray
+ float light_march_dx; // distance between steps while marching along the light ray
+ float light_march_x; // distance traversed while marching along the light ray
+ float light_march_h; // distance ("height") from the surface of the world while marching along the light ray
+ vec3 light_march_pos= vec3(0); // absolute position while marching along the light ray
+ bool is_interaction = try_get_relation_between_ray_and_sphere(
+  view_origin, view_direction,
+  world_position, world_radius + atmosphere_height,
+  view_r_closest2, view_x_closest,
+  view_x_enter, view_x_exit
+ );
+ view_march_dx = (view_x_exit - view_x_enter) / VIEW_MARCH_STEP_COUNT;
+ view_march_x = view_x_enter + 0.5 * view_march_dx;
+ for (float i = 0.; i < VIEW_MARCH_STEP_COUNT; ++i)
+ {
+  view_march_pos = view_origin + view_direction * view_march_x;
+  view_march_h = length(view_march_pos - world_position) - world_radius;
+  // NOTE: we do not capture the return value since at this point we're always guaranteed to be in the atmosphere
+  try_get_relation_between_ray_and_sphere(
+   view_march_pos, light_direction,
+   world_position, world_radius + atmosphere_height,
+   light_r_closest2,light_x_closest,
+   light_x_enter, light_x_exit
+  );
+     light_march_dx = light_x_exit / LIGHT_MARCH_STEP_COUNT;
+  light_march_x = 0.5 * light_march_dx;
+  for (float j = 0.; j < LIGHT_MARCH_STEP_COUNT; ++j)
+  {
+   light_march_pos = view_march_pos + light_direction * light_march_x;
+   light_march_h = length(light_march_pos - world_position) - world_radius;
+   light_march_x += light_march_dx;
+  }
+  view_march_x += view_march_dx;
+ }
  return vec3(0);
 }
 void main() {
@@ -553,7 +603,6 @@ void main() {
     vec2 clipspace = 2.0 * screenspace - 1.0;
  vec3 ray_direction = normalize(view_matrix_inverse * projection_matrix_inverse * vec4(clipspace, 1, 1)).xyz;
  vec3 ray_origin = view_matrix_inverse[3].xyz * reference_distance;
- // ray_origin ;
  // NOTE: 3 scale heights should capture 95% of the atmosphere's mass, 
  //   enough to be aesthetically appealing.
  float atmosphere_height = 3. * max(scale_heights.x, scale_heights.y);
