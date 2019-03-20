12d6ccff7cd563f0340a462370b5d951a161a80e
Carl Davidson <carl.davidson@pioneer.com>
2019-03-20 17:31:31 -0500

    Implementing image import feature for issue #15

--- noncompiled/generators/CrustGenerator.js
+++ noncompiled/generators/CrustGenerator.js
@@ -1,9 +1,22 @@
 'use strict';
 
 var CrustGenerator = {};
-CrustGenerator.generate = function (height_ranks, hypsography, attribute_height_maps, crust, random) {
-
-    // order cells by this new found  "height rank"
+CrustGenerator.get_crust_from_elevations = function(elevations, attribute_height_maps, crust) {
+    // Our model does not work directly with elevation.
+    // We must express elevation in terms of thickness/density
+    // To do this, we start off with a set of rock column templates expressing
+    // what thickness/density should look like at a given density.
+    // We then interpolate between these templated values.
+    attribute_height_maps.sediment             (elevations, crust.sediment        );
+    attribute_height_maps.sedimentary          (elevations, crust.sedimentary     );
+    attribute_height_maps.metamorphic          (elevations, crust.metamorphic     );
+    attribute_height_maps.felsic_plutonic      (elevations, crust.felsic_plutonic );
+    attribute_height_maps.felsic_volcanic      (elevations, crust.felsic_volcanic );
+    attribute_height_maps.mafic_volcanic       (elevations, crust.mafic_volcanic  );
+    attribute_height_maps.age                  (elevations, crust.age             );
+}
+CrustGenerator.get_elevations_from_height_ranks = function (height_ranks, hypsography, random, elevations) {
+    // order cells by height rank
     var sorted_cell_ids = new Uint16Array(height_ranks.length);
     for(var i=0, length = sorted_cell_ids.length; i<length; i++) {
         sorted_cell_ids[i] = i;
@@ -21,23 +34,16 @@ CrustGenerator.generate = function (height_ranks, hypsography, attribute_height_
     sorted_height_samples.sort(function(a,b) { return a-b; });
 
     // Now use the cell ids height ranks 
-    var heights = Float32Raster.FromExample(height_ranks);
+    var elevations = elevations || Float32Raster.FromExample(height_ranks);
     for (var i = 0, li = sorted_cell_ids.length; i < li; i++) {
-        heights[sorted_cell_ids[i]] = sorted_height_samples[i];
+        elevations[sorted_cell_ids[i]] = sorted_height_samples[i];
     }
 
-    // Our model does not work directly with elevation.
-    // We must express elevation in terms of thickness/density
-    // To do this, we start off with a set of rock column templates expressing
-    // what thickness/density should look like at a given density.
-    // We then interpolate between these templated values.
-    attribute_height_maps.sediment             (heights, crust.sediment        );
-    attribute_height_maps.sedimentary        (heights, crust.sedimentary        );
-    attribute_height_maps.metamorphic        (heights, crust.metamorphic        );
-    attribute_height_maps.felsic_plutonic    (heights, crust.felsic_plutonic    );
-    attribute_height_maps.felsic_volcanic    (heights, crust.felsic_volcanic    );
-    attribute_height_maps.mafic_volcanic    (heights, crust.mafic_volcanic    );
-    attribute_height_maps.age                (heights, crust.age                );
+    return elevations;
+}
+CrustGenerator.get_crust_from_height_ranks = function (height_ranks, hypsography, attribute_height_maps, random, crust) {
+    var elevations = CrustGenerator.get_elevations_from_height_ranks(height_ranks, hypsography, random);
+    return CrustGenerator.get_crust_from_elevations(elevations, attribute_height_maps, crust);
 };
 
 CrustGenerator.early_earth_hypsography = function(random) {
