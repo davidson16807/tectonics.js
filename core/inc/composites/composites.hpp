276d3dbe5cfbc0685c58c502a1872fed64ce947c
Carl Davidson <carl.davidson@pioneer.com>
2019-04-01 16:05:13 -0500

    got as far as I could while still able to compile, can't go much further

--- core/inc/composites/composites.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "many.hpp"
-#include "common.hpp"
-#include "exponential.hpp"
-#include "morphologic.hpp"
-#include "trigonometric.hpp"
-#include "statistic.hpp"
--- core/inc/composites/glm/glm.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "vecs.hpp"
-#include "vector_relational.hpp"
-#include "common.hpp"
-#include "geometric.hpp"
-#include "statistic.hpp"
