6c8886dad0dea5919be8510454d32bedc14ca2e0
Carl Davidson <davids14@illinoisalumni.org>
2019-04-13 16:34:19 -0500

    fleshing out test driver for rasters and composites

--- core/tests/driver.cpp
+++ core/tests/driver.cpp
@@ -1,13 +1,79 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+
+#define GLM_FORCE_PURE      // disable SIMD support for glm so we can work with webassembly
+#include <glm/vec2.hpp>     // vec2, bvec2, dvec2, ivec2 and uvec2
+#include <glm/vec3.hpp>     // vec3, bvec3, dvec3, ivec3 and uvec3
+#include <glm/vec4.hpp>     // vec4, bvec4, dvec4, ivec4 and uvec4
+#include <glm/matrix.hpp>     // vec4, bvec4, dvec4, ivec4 and uvec4
+#include <composites/composites.hpp>     // vec2, bvec2, dvec2, ivec2 and uvec2
+#include <composites/glm/glm.hpp>     // vec*, bvec*, dvec*, ivec* and uvec*
+
+#include "rasters/SphereGridVoronoi3d.hpp"
+#include "rasters/CartesianGridCellList3d.hpp"
+#include "rasters/Grid.hpp"
+
+#include "academics/tectonics.hpp"
+
+// #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch/catch.hpp"
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
+using namespace composites;
+using namespace rasters;
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+Grid tetrahedron = Grid(
+        vec3s({
+                vec3(0,0,0),
+                vec3(1,0,0),
+                vec3(0,1,0),
+                vec3(0,0,1)
+            }),
+        uvec3s({
+                uvec3(0,1,2),
+                uvec3(0,1,3),
+                uvec3(0,2,3),
+                uvec3(1,2,3)
+            })
+    );
+// "diamond" is a 2d grid, it looks like this:
+//   2  
+//  /|\ 
+// 3-0-1
+//  \|/ 
+//   4   
+Grid diamond = Grid(
+        vec3s({
+                vec3( 0, 0, 0),
+                vec3( 1, 0, 0),
+                vec3( 0, 1, 0),
+                vec3(-1, 0, 0),
+                vec3( 0,-1, 0)
+            }),
+        uvec3s({
+                uvec3(0,1,2),
+                uvec3(0,1,4),
+                uvec3(0,3,2),
+                uvec3(0,3,4)
+            })
+    );
+
+int main(int argc, char const *argv[])
+{
+    //   0  
+    //  /|\ 
+    // 0-1-1
+    //  \|/ 
+    //   0   
+    bools plate_mask = bools ({1,1,0,0,0});
+    //   0  
+    //  /|\ 
+    // 0-0-1
+    //  \|/ 
+    //   0   
+    floats buoyancy  = floats({0,1,0,0,0});
+    float mantle_viscosity = 1.57e20;
+    vec3s gradient_of_bools = vec3s(4);
+    gradient(plate_mask, tetrahedron, gradient_of_bools);
+    academics::tectonics::guess_plate_velocity(plate_mask, buoyancy, mantle_viscosity, diamond, gradient_of_bools);
+    std::cout << gradient_of_bools;
+    return 0;
 }
