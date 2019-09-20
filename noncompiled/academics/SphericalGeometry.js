18fb62d0c9f14a27d42ed8cdfecdf9d0b9497c37
Carl Davidson <carl.davidson@pioneer.com>
2019-09-20 13:15:52 -0600

    simplifying logic for get_random_surface_field()
    discovered after trying to port to C++

--- noncompiled/academics/SphericalGeometry.js
+++ noncompiled/academics/SphericalGeometry.js
@@ -89,23 +89,6 @@ var SphericalGeometry = (function() {
     // instead of an immediate drop off between sides. 
     // This is done to produce smoother terrain using fewer iterations 
     SphericalGeometry.get_random_surface_field = function (grid, random) {
-        var exp = Math.exp;
-
-        function heaviside_approximation (x, k) {
-            return 2 / (1 + exp(-k*x)) - 1;
-            return x>0? 1: 0; 
-        }
-
-        // first, we generate matrices expressing direction of continent centers
-        // Only the z axis is used to determine distance to a continent's center,
-        // so we discard all but the row representing the z axis
-        // this row is stored as a vector, and we take the dot product with the cell pos 
-        // to find the z axis relative to the continent center 
-        var zDotMultipliers = [];
-        for (var i = 0; i < 1000; i++) {
-            zDotMultipliers.push(SphericalGeometry.get_random_point_on_surface(random));
-        };
-
         // Now, we iterate through the cells and find their "height rank".
         // This value doesn't translate directly to elevation. 
         // It only represents how cells would rank if sorted by elevation.
@@ -115,8 +98,8 @@ var SphericalGeometry = (function() {
         var z = Float32Raster(grid);
         var height_ranks = Float32Raster(grid);
         Float32Raster.fill(height_ranks, 0);
-        for (var j = 0, lj = zDotMultipliers.length; j < lj; j++) {
-            VectorField.dot_vector(pos, zDotMultipliers[j], z);
+        for (var j = 0, lj = 1000; j < lj; j++) {
+            VectorField.dot_vector(pos, SphericalGeometry.get_random_point_on_surface(random), z);
             Float32RasterInterpolation.smoothstep2(z, 300, z);
             ScalarField.add_field(height_ranks, z, height_ranks);
         }
