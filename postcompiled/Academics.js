bb16f7b5cb61f3f1bd4cc48f4059271ab8dd624e
Carl <diadophus@gmail.com>
2020-02-21 00:43:16 -0700

    fixing issue with negation that I think was causing scatterer banding

--- postcompiled/Academics.js
+++ postcompiled/Academics.js
@@ -268,11 +268,20 @@ maybe_vec2 get_distances_along_line_to_negation(
     in maybe_vec2 positive,
     in maybe_vec2 negative
 ) {
-    return maybe_vec2(
-        glm.vec2(!negative.exists ? positive.value.x : min(negative.value.y, positive.value.x),
-             !negative.exists || negative.value.y < 0. ? positive.value.y : min(negative.value.x, positive.value.y)),
-        positive.exists && !(negative.value.x < positive.value.x && positive.value.y < negative.value.y)
-    );
+    // as long as intersection with positive exists, 
+    // and negative doesn't completely surround it, there will be an intersection
+    bool exists =
+        positive.exists && !(negative.value.x < positive.value.x && positive.value.y < negative.value.y);
+    // find the first region of intersection
+    float entrance = !negative.exists ? positive.value.x : min(negative.value.y, positive.value.x);
+    float exit = !negative.exists ? positive.value.y : min(negative.value.x, positive.value.y);
+    // if the first region is behind us, find the second region
+    if (exit < 0. && 0. < positive.value.y)
+    {
+        entrance = negative.value.y;
+        exit = positive.value.y;
+    }
+    return maybe_vec2( glm.vec2(entrance, exit), exists );
 }
 // "oplus" is the o-plus operator,
 //   or the reciprocal of the sum of reciprocals.
