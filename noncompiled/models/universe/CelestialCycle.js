034ed322246cc4b7852d583f24676552a650c725
Carl <diadophus@gmail.com>
2020-03-21 17:30:45 -0600

    fixing issue with serializing celestial cycles

--- noncompiled/models/universe/CelestialCycle.js
+++ noncompiled/models/universe/CelestialCycle.js
@@ -1,14 +1,17 @@
 'use strict';
 
-// A "CelestialCycle" is a class representation of an isolated physical system driven by a cycle
-// it is essentially a node in a scene graph 
-// It is designed for on-rails physics simulation over large distances.
-// It offers support for constantly changing transformation matrices,
-// and allows arbitrary nodes to be designated as the origin of a coordinate system.
-// Designating arbitrary nodes as the origin is meant to resolve floating point precision issues 
-// that commonly occur for very distant objects, A.K.A. the "Deep Space Kraken" of Kerbal Space Program
+/*
+A "CelestialCycle" is a class representation of an isolated physical system driven by a cycle
+it is essentially a node in a scene graph 
+It is designed for on-rails physics simulation over large distances.
+It offers support for constantly changing transformation matrices,
+and allows arbitrary nodes to be designated as the origin of a coordinate system.
+Designating arbitrary nodes as the origin is meant to resolve floating point precision issues 
+that commonly occur for very distant objects, A.K.A. the "Deep Space Kraken" of Kerbal Space Program
+*/
 function CelestialCycle(parameters) {
     this.id = parameters.id;
+    this.name = parameters.name || parameters.id;
 
     if (parameters.motion === void 0) {
         stop('missing parameter: "motion"');
@@ -16,26 +19,34 @@ function CelestialCycle(parameters) {
     if (parameters.motion.type === void 0) {
         stop('missing parameter: "motion.type"');
     }
-    // the motion that characterizes all bodies within the cycle
-    // motion can currently either be an "Orbit" or "Spin", although it could be any class that instantiates their methods
+    /*
+    the motion that characterizes all bodies within the cycle
+    motion can currently either be an "Orbit" or "Spin", although it could be any class that instantiates their methods
+    */
     this.motion = {
         'orbit': () => new Orbit(parameters.motion),
         'spin': () => new Spin(parameters.motion),
     }[parameters.motion.type]();
 
-    // the body that exhibits the motion (optional)
-    // the position/rotation of the body is described by a coordinate basis that is designated by this node
-    // remember that a path need not always have a body - 
-    // it may for instance describe a group of objects that are gravitationally bound
+    /*
+    the body that exhibits the motion (optional)
+    the position/rotation of the body is described by a coordinate basis that is designated by this node
+    remember that a path need not always have a body - 
+    it may for instance describe a group of objects that are gravitationally bound
+    */
     this.body = parameters.body;
 
-    // the parent motion of the scene graph node (optional)
-    // the motion described by this.motion assumes a coordinate basis that is designated by the parent node
-    // TODO: maybe set this to a dependency? it is assigned by the Universe object, after all
+    /*
+    the parent motion of the scene graph node (optional)
+    the motion described by this.motion assumes a coordinate basis that is designated by the parent node
+    TODO: maybe set this to a dependency? it is assigned by the Universe object, after all
+    */
     this.parent     = parameters.parent;
 
-    // the child motions of the scene graph node (optional)
-    // the motions described by the children assume a coordinate basis that is designated by this node
+    /*
+    the child motions of the scene graph node (optional)
+    the motions described by the children assume a coordinate basis that is designated by this node
+    */
     this.children = (parameters.children || []);
 
     // whether or not the insolation of child bodies will change throughout this cycle's motion
@@ -43,9 +54,11 @@ function CelestialCycle(parameters) {
 
     this.getParameters = function() {
         return {
-            name:         this.id,
+            id:           this.id,
+            name:         this.name,
             motion:       this.motion.getParameters(),
             body:         this.body,
+            parent:       this.parent,
             children:     this.children,
             invariant_insolation: this.invariant_insolation,
         };
@@ -53,8 +66,10 @@ function CelestialCycle(parameters) {
 
     var mult_matrix = Matrix4x4.mult_matrix;
     
-    // returns a dictionary mapping body ids to transformation matrices
-    //  indicating the position/rotation relative to this node 
+    /*
+    returns a dictionary mapping body ids to transformation matrices
+     indicating the position/rotation relative to this node 
+    */
     this.get_body_matrices = function (config, cycles, origin) {
         origin = origin || this.id;
         var parent   = this.parent;
